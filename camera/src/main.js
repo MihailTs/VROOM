d17f98170e25241bf734e303fae1219a67d82e76
Deyan Delchev <deyandelchev03@gmail.com>
2025-03-16 00:57:01 +0200

    front and back camera

--- camera/src/main.js
+++ camera/src/main.js
@@ -1,67 +1,92 @@
 import { io } from "socket.io-client";
 import { Peer } from "peerjs";
 
-const myPeer = new Peer();
+let cam = null;
 
-const SERVER = import.meta.env.VITE_SIGNALING_SERVER;
+const app = document.getElementById("app")
+const front = document.getElementById("front");
+const back = document.getElementById("back");
 
-const socket = io(SERVER, {
-  reconnectionDelayMax: 10000,
-});
+function clearButtons() {
+  app.removeChild(front)
+  app.removeChild(back)
+}
 
-myPeer.on("open", () => {
-  console.log("Camera App Peer ID:", myPeer.id);
+function cameraStream() {
+  const myPeer = new Peer();
 
-  socket.emit("camera join", myPeer.id);
+  const SERVER = import.meta.env.VITE_SIGNALING_SERVER;
 
-  socket.on("camera", (clientId) => {
-    navigator.mediaDevices
-      .getUserMedia({
-        video: {
-          facingMode: "environment",
-        },
-        audio: false
-      })
-      .then((localStream) => {
-        const call = myPeer.call(clientId, localStream);
-
-        window.addEventListener("beforeunload", (event) => {
-          event.preventDefault();
-          call.close();
-        });
-
-        console.log("Connected to ", clientId);
-
-        call.on("close", () => {
-          console.log("Call closed");
-        });
-
-        call.on("error", (err) => {
-          console.log("Call error:", err.message);
-        });
-      })
-      .catch((err) => {
-        console.error("Error accessing camera:", err);
-      });
+  const socket = io(SERVER, {
+    reconnectionDelayMax: 10000,
   });
 
-  socket.on("camera broadcast", (clientId) => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then((localStream) => {
-        const call = myPeer.call(clientId, localStream);
-        console.log("Connected to ", clientId);
+  myPeer.on("open", () => {
+    console.log("Camera App Peer ID:", myPeer.id);
 
-        call.on("close", () => {
-          console.log("Call closed");
-        });
+    socket.emit("camera join", myPeer.id);
 
-        call.on("error", (err) => {
-          console.log("Call error:", err.message);
+    socket.on("camera", (clientId) => {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: {
+            facingMode: cam,
+          },
+          audio: false,
+        })
+        .then((localStream) => {
+          const call = myPeer.call(clientId, localStream);
+
+          window.addEventListener("beforeunload", (event) => {
+            event.preventDefault();
+            call.close();
+          });
+
+          console.log("Connected to ", clientId);
+
+          call.on("close", () => {
+            console.log("Call closed");
+          });
+
+          call.on("error", (err) => {
+            console.log("Call error:", err.message);
+          });
+        })
+        .catch((err) => {
+          console.error("Error accessing camera:", err);
         });
-      })
-      .catch((err) => {
-        console.error("Error accessing camera:", err);
-      });
+    });
+
+    socket.on("camera broadcast", (clientId) => {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((localStream) => {
+          const call = myPeer.call(clientId, localStream);
+          console.log("Connected to ", clientId);
+
+          call.on("close", () => {
+            console.log("Call closed");
+          });
+
+          call.on("error", (err) => {
+            console.log("Call error:", err.message);
+          });
+        })
+        .catch((err) => {
+          console.error("Error accessing camera:", err);
+        });
+    });
   });
-});
+}
+
+front.onclick = (e) =>{
+  cam = "user"
+  clearButtons()
+  cameraStream()
+}
+
+back.onclick = (e) =>{
+  cam = "environment"
+  clearButtons()
+  cameraStream()
+}
