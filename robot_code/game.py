ffa18768a83c3f627e0e4e8d41568aac33fd6b1d
Boris Vassilev <boris.n.vassilev@gmail.com>
2025-03-16 02:56:32 +0200

    two vrooms rotate

--- robot_code/game.py
+++ robot_code/game.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from math import *
 
 pygame.init()
 
@@ -9,42 +10,67 @@ dt = 0
 clock = pygame.time.Clock()
 FPS = 60
 
-<<<<<<< HEAD
-=======
+speed = 100;
+rotationSpeed = 100;
 delayQueue = []
 
 
->>>>>>> 4bf8641dc3ffb2350ae62d63b8caad6529206d64
 class VroomPrediction (pygame.sprite.Sprite):
     def __init__(self):
         super(VroomPrediction, self).__init__()
         self.image = pygame.image.load("vroom.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (32, 32))
+        self.image = pygame.transform.scale(self.image, (64, 64))
+        self.realImage = self.image
         self.rect = self.image.get_rect()
         self.position = pygame.math.Vector2(320, 240)
+        self.rotation = 0
+        self.w = 0
         self.velocity = pygame.math.Vector2(0, 0)
     
     def update(self):
         self.position += self.velocity * dt
-        self.rect.update(self.position.x, self.position.y, 32, 32)
+        self.rotation += self.w * dt
+        self.realImage = pygame.transform.rotate(self.image, self.rotation)
+        self.rect.update(self.position.x, self.position.y, self.rect.width, self.rect.height)
     
     def move_straight(self):
-        self.velocity = pygame.math.Vector2(0, -100)
+        rotation = self.rotation * pi / 180
+        self.velocity = pygame.math.Vector2(
+                -speed * sin(rotation), -speed * cos(rotation)
+        )
 
     def move_reverse(self):
-        self.velocity = pygame.math.Vector2(0, 100)
+        rotation = self.rotation * pi / 180
+        self.velocity = pygame.math.Vector2(
+                speed * sin(rotation), speed * cos(rotation)
+        )
+    
+    def turn_left(self):
+        self.w = rotationSpeed
+
+    def turn_right(self):
+        self.w = -rotationSpeed
 
     def stop(self):
-        self.velocity = pygame.math.Vector2(0, 0)
+        pass
 
     def key(self, event):
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                self.move_straight();
-            if event.key == pygame.K_DOWN:
-                self.move_reverse();
+            if self.velocity.length() <= 0.01 and abs(self.w) <= 0.01:
+                if event.key == pygame.K_UP:
+                    self.move_straight();
+                if event.key == pygame.K_DOWN:
+                    self.move_reverse();
+                if event.key == pygame.K_LEFT:
+                    self.turn_left();
+                if event.key == pygame.K_RIGHT:
+                    self.turn_right();
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                self.velocity = pygame.math.Vector2(0, 0)
+                self.stop()
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                self.w = 0
                 self.stop()
 
 vroom = VroomPrediction()
@@ -60,25 +86,19 @@ while True:
             if event.key == pygame.K_q:
                 pygame.quit()
                 sys.exit()
-<<<<<<< HEAD
-            vroom.keydown(event.key)
-        if event.type == pygame.KEYUP:
-            vroom.keyup(event.key)
-        delayqueue.push(event)
-=======
         vroom.key(event)
         delayQueue.append([event, current_time + 2000])
+
     while(delayQueue and delayQueue[0][1] <= current_time):
         event = delayQueue.pop(0)[0]
         real_vroom.key(event)
     
->>>>>>> 4bf8641dc3ffb2350ae62d63b8caad6529206d64
     screen.fill((255, 255, 255))
 
     vroom.update()
     real_vroom.update()
-    screen.blit(vroom.image, vroom.rect)
-    screen.blit(real_vroom.image, real_vroom.rect)
+    screen.blit(vroom.realImage, vroom.rect)
+    screen.blit(real_vroom.realImage, real_vroom.rect)
 
     dt = clock.tick(FPS) / 1000
     pygame.display.update()
